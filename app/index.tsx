8a0dae5c0324792e6d7424437f8ab78881dd62b6
khanghtse <khanghoang090703@gmail.com>
2024-08-12 08:43:54 +0700

    meditation-app

---
+++ app/index.tsx
@@ -0,0 +1,47 @@
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import React from "react";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
+import AppGradient from "@/components/AppGradient";
+const App = () => {
+  const router = useRouter();
+
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+          
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-regular text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton 
+                onPress={() => router.push("/nature-meditate")}  
+                title= "Get started"
+              />
+            </View>
+
+            <StatusBar style="light"/>
+          </SafeAreaView>
+        </AppGradient>
+        
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default App;
---
+++ components/GuidedAffirmationGallery.tsx
@@ -0,0 +1,45 @@
+import { View, Text, FlatList, Pressable, Image } from "react-native";
+import React from "react";
+import { GalleryPreviewData } from "@/constants/models/AffirmationCategory";
+import { Link } from "expo-router";
+
+interface GuidedAffirmationGalleryProps {
+  title: string;
+  previews: GalleryPreviewData[];
+}
+const GuidedAffirmationGallery = ({
+  title,
+  previews,
+}: GuidedAffirmationGalleryProps) => {
+  return (
+    <View className="my-5">
+      <View className="mb-2">
+        <Text className="text-white font-bold text-xl">{title}</Text>
+      </View>
+
+      <View className="space-y-2">
+        <FlatList
+          data={previews}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Link href={`/affirmation/${item.id}`} asChild>
+              <Pressable>
+                <View className="h-36 w-32 rounded-md mr-4">
+                  <Image
+                    source={item.image}
+                    className="w-full h-full"
+                    resizeMode="cover"
+                  />
+                </View>
+              </Pressable>
+            </Link>
+          )}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default GuidedAffirmationGallery;
